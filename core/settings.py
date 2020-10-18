806a1fd58f47b378272db2f90ddc2b1789d80eee
André P. Santos <andreztz@gmail.com>
2020-10-17 23:20:04 -0300

    Adiciona python decouple.

--- core/settings.py
+++ core/settings.py
@@ -11,24 +11,23 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import decouple
+from dj_database_url import parse as database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yanksf+sh%wi8odb$neut9f=@*gjku7ehre$llhx25*4$lj#=d'
-
+SECRET_KEY = decouple.config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = decouple.config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
+ALLOWED_HOSTS = decouple.config(
+    "ALLOWED_HOSTS", default=[], cast=decouple.Csv()
+)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -79,14 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+DEFAULT_DATABASE_URL = "sqlite:////" + os.path.join(BASE_DIR, "db.sqlite3")
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": decouple.config(
+        "DATABASE_URL", default=DEFAULT_DATABASE_URL, cast=database_url
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -106,38 +103,33 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
-
 LANGUAGE_CODE = 'pt-br'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
 LOGIN_REDIRECT_URL = 'home'
-
 LOGOUT_REDIRECT_URL = 'home'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = 'media'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [
     'statics'
 ]
-
 STATIC_URL = '/static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = decouple.config('EMAIL_HOST')
+EMAIL_PORT = decouple.config('EMAIL_PORT')
+EMAIL_USE_TLS = decouple.config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = decouple.config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = decouple.config('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = decouple.config(
+    'EMAIL_BACKEND',
+    default='django.core.mail.backends.smtp.EmailBackend'
+)
